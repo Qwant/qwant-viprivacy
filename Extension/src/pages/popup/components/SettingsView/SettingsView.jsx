e54b525a19b0187e9ebc3d88e0d9f257fe8e9c50
Jonathan <contact@grafikart.fr>
2022-06-29 17:00:56 +0200

    ref: permission level ui

---
+++ Extension/src/pages/popup/components/SettingsView/SettingsView.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react';
+
+import { messenger } from '~src/pages/services/messenger';
+import { reactTranslator } from '~src/common/translators/reactTranslator';
+
+import { Stack } from '@qwant/qwant-ponents';
+import { CheckboxCard } from '~src/pages/common/components/CheckboxCard/CheckboxCard';
+import { PopupView } from '../components/PopupView';
+
+const SettingsView = observer(({ store, settingsStore }) => {
+    const { applicationFilteringDisabled } = store;
+    const { protectionLevel } = settingsStore;
+
+    const [activeLevel, setActiveLevel] = useState(() => {
+        if (applicationFilteringDisabled) return 'disabled';
+        return protectionLevel;
+    });
+
+    useEffect(() => {
+        if (!activeLevel && !!protectionLevel) {
+            setActiveLevel(protectionLevel);
+        }
+    }, [activeLevel, protectionLevel]);
+
+    const onProtectionDisable = async () => {
+        await store.changeApplicationFilteringDisabled(!applicationFilteringDisabled);
+    };
+
+    const levelChangeHandler = (level) => async (e) => {
+        e.preventDefault();
+        const transaction = window?.apm?.startTransaction(`protection-button-click-${level}`);
+        setActiveLevel(level);
+        if (level === 'disabled') {
+            await onProtectionDisable();
+            return;
+        }
+
+        await store.changeApplicationFilteringDisabled(false);
+
+        messenger.changeProtectionLevel(level);
+        settingsStore.setProtectionLevel(level);
+
+        if (transaction) {
+            transaction.result = 'success';
+            transaction.end();
+        }
+    };
+
+    const title = reactTranslator.getMessage('protection_level');
+    const levels = ['standard', 'strict', 'disabled'];
+
+    return (
+        <PopupView title={title}>
+            <Stack gap="xs">
+                {levels.map((level) => (
+                    <CheckboxCard
+                        compact
+                        key={level}
+                        title={reactTranslator.getMessage(`protection_level_${level}`)}
+                        description={reactTranslator.getMessage(`protection_level_${level}_description`)}
+                        onClick={levelChangeHandler(level)}
+                        selected={activeLevel === level}
+                    />
+                ))}
+            </Stack>
+        </PopupView>
+    );
+});
+
+export default SettingsView;
