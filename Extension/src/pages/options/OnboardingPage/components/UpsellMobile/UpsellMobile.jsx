e6077141333f7db258a5cec51849bffc8db35022
Raed667 <github@raed.email>
2022-07-06 18:42:49 +0200

    i18n Button download apps

--- Extension/src/pages/options/OnboardingPage/components/UpsellMobile/UpsellMobile.jsx
+++ Extension/src/pages/options/OnboardingPage/components/UpsellMobile/UpsellMobile.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 import { Card, Stack, Text } from '@qwant/qwant-ponents';
 import { t } from '~src/common/translators/reactTranslator';
 import Styles from './UpsellMobile.module.scss';
-import ButtonAndroid from '../assets/buttons-apps-google-negative.svg';
-import ButtonIOS from '../assets/buttons-apps-apple-negative.svg';
 import QRCode from '../assets/qr-code.png';
+
+import { ButtonDlApp } from './ButtonDlApp';
 
 export function UpsellMobile() {
     return (
         <Card className={Styles.UpsellMobile} p="xl">
             <div className={Styles.LeftImage}>
-                <img src="https://place-hold.it/220x220" alt="" />
+                <img src="https://place-hold.it/196x196" alt="" />
             </div>
             <Stack gap="m">
                 <Text typo="heading-5" color="primary" bold>
                     {t('upsell_mobile_title')}
                 </Text>
                 <Stack horizontal gap="xs">
-                    <a target="_blank" rel="noreferrer" href="https://play.google.com/store/apps/details?id=com.qwant.liberty">
-                        <img src={ButtonAndroid} alt="" />
-                    </a>
-                    <a target="_blank" rel="noreferrer" href="https://apps.apple.com/fr/app/qwant/id924470452">
-                        <img src={ButtonIOS} alt="" />
-                    </a>
+                    <ButtonDlApp />
+                    <ButtonDlApp isIos />
                 </Stack>
             </Stack>
             <Stack p="xs" gap="xs" className={Styles.UpsellMobileQRCode}>
