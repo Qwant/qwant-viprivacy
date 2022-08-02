f1ce10e19053013e8574d4100a7fb282354c993f
Raed667 <github@raed.email>
2022-08-02 11:44:36 +0200

    localize onboarding image

--- Extension/src/pages/options/OnboardingPage/components/UpsellMobile/UpsellMobile.jsx
+++ Extension/src/pages/options/OnboardingPage/components/UpsellMobile/UpsellMobile.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Card, Stack, Text } from '@qwant/qwant-ponents';
 import { t } from '~src/common/translators/reactTranslator';
+import { backgroundPage } from '~src/background/extension-api/background-page';
 import Styles from './UpsellMobile.module.scss';
 import QRCode from '../assets/qr-code.png';
-import UpsellMobileIllustration from '../assets/upsell-mobile.png';
+
+import imageUrlEN from '../assets/upsell-mobile.png';
+import imageUrlFR from '../assets/upsell-mobile-fr.png';
 
 import { ButtonDlApp } from './ButtonDlApp';
+
+const locale = backgroundPage.app.getLocale();
+
+const imageUrl = locale === 'fr' ? imageUrlFR : imageUrlEN;
 
 export function UpsellMobile() {
     return (
         <Card className={Styles.UpsellMobile} pr="xl">
             <div className={Styles.LeftImage}>
-                <img src={UpsellMobileIllustration} width="196" height="196" alt="" />
+                <img src={imageUrl} width="196" height="196" alt="" />
             </div>
             <Stack gap="m">
                 <Text typo="heading-5" color="primary" bold>
